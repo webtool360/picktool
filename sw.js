55f41a212477330e523371d5ee1ba7153f039506
Muhammad Nasir Afridi <mnasirafridi313@gmail.com>
2026-07-11 10:04:35 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('picktool-store').then((cache) => {
+      return cache.addAll([
+        '/', 
+        '/index.html', 
+        '/manifest.json', 
+        '/IMG_20260710_150257.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
 
